a57cb071d1d312c8e5bf6d0206efc9b874825fba
Vasilia Staykova <someonemyfriend@gmail.com>
2023-01-18 02:24:48 +0200

    Add files via upload

--- src/js/app.js
+++ src/js/app.js
@@ -3,13 +3,19 @@
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
   let elements = document.querySelectorAll(".hot");
+ console.log(elements);
+ 
+// elements.textContent = '🔥';
+
   if(elements){
     console.log(elements);
     for(let element of elements){
       let child = document.createElement("i");
       child.setAttribute("class", 'fa fa-fire');
-      element.appendChild(child);
+      element.appendChild(child).textContent = '🔥';;
     }
   }
 
-});
+}
+
+);
